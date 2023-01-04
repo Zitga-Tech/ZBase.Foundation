62d28cb8a653950a035985e87d185b9370ce5217
Grashaar <grashaar@outlook.com>
2023-01-04 19:07:12 +0700

    Add mechanism to retrieve names from enum values

--- Packages/ZBase.Foundation/ZBase.Foundation/Common/Enum{T}.cs
+++ Packages/ZBase.Foundation/ZBase.Foundation/Common/Enum{T}.cs
@@ -1,29 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ZBase.Foundation
 {
     public static class Enum<T>
-        where T : unmanaged, System.Enum
+        where T : unmanaged, Enum
     {
-        private static readonly System.Type s_type;
-        private static readonly System.Type s_underlyingType;
+        private static readonly Type s_type;
+        private static readonly Type s_underlyingType;
         private static readonly T[] s_values;
         private static readonly string[] s_names;
+
+        private static Dictionary<T, string> s_nameMap;
 
         static Enum()
         {
             s_type = TypeCache.Get<T>();
-            s_underlyingType = System.Enum.GetUnderlyingType(s_type);
-            s_values = (T[])System.Enum.GetValues(s_type);
-            s_names = System.Enum.GetNames(s_type);
+            s_underlyingType = Enum.GetUnderlyingType(s_type);
+            s_values = (T[])Enum.GetValues(s_type);
+            s_names = Enum.GetNames(s_type);
+
+            Init();
         }
 
-        public static System.Type UnderlyingType => s_underlyingType;
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        static void Init()
+        {
+            var nameMap = s_nameMap ??= new Dictionary<T, string>();
+            var values = s_values;
+            var names = s_names;
+            var length = values.Length;
+
+            nameMap.Clear();
+
+            for (var i = 0; i < length; i++)
+            {
+                nameMap[values[i]] = names[i];
+            }
+        }
+
+        public static Type UnderlyingType => s_underlyingType;
 
         public static ReadOnlyMemory<T> Values => s_values;
 
         public static ReadOnlyMemory<string> Names => s_names;
 
         public static int ValueCount => s_values.Length;
+
+        public static string GetName(T value)
+        {
+            if (s_nameMap.TryGetValue(value, out var name))
+            {
+                return name;
+            }
+
+            return string.Empty;
+        }
     }
 }
